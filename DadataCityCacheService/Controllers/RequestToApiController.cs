2d254fd606550de219d30c1533d5105c432e4e7a
akhmetov_rr <akhmetov_rr@nartis.ru>
2024-06-07 18:21:44 +0300

    first iteration

--- DadataCityCacheService/Controllers/RequestToApiController.cs
+++ DadataCityCacheService/Controllers/RequestToApiController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-
 using Dadata;
 using Dadata.Model;
-using System.Net;
+using System.Text;
+using DadataCityCacheService.Models;
 
 namespace DadataCityCacheService.Controllers
 {
@@ -10,31 +10,63 @@ namespace DadataCityCacheService.Controllers
     [Route("[controller]")]
     public class DadaCacheController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<DadaCacheController> _logger;
-
-        public DadaCacheController(ILogger<DadaCacheController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public async Task<IActionResult> Get([FromBody] string request)
+        public async Task<ActionResult<string[]>> Get([FromBody] string request)
         {
             var token = "055fa6c2af4562aeee76ada479f00b2218610bd0";
             var secret = "2a984b6d1c9211c0ba9f209148a5f73f0a7c4a34";
 
             var api = new CleanClientAsync(token, secret);
-            // or any of the following, depending on the API method
-            //api = new SuggestClientAsync(token);
-            //api = new OutwardClientAsync(token);
-            //api = new ProfileClientAsync(token, secret);
 
             var address = await api.Clean<Address>(request);
+
+
+            if (!await CheckResponseCorrect()) throw new();
+
+
+
+
+            var cityInfoOnly = await CheckThisInfoCityOnly(address);
+
+            string[] result;
+
+            if (cityInfoOnly)
+            {
+                result = TryGetCachedInfo(address, out CityInfoOnly cachedInfo) ? cachedInfo.ToArray() : new string[]
+                    {
+                    "dfsf"
+                    };
+
+                return result;
+            }
+
+
+
+
+
+
+
+            //if(await CheckThisInfoCityOnly(address))
+            //{
+            //    result.Add(address.fias_id);
+            //    result.Add(address.result);
+            //    result.Add(address.geo_lat);
+            //    result.Add(address.geo_lon);
+            //    result.Add(address.timezone);
+            //}
+            //else
+            //{
+            //    result =
+            //        new()
+            //        {
+            //            address.fias_id,
+            //            address.region_with_type,
+            //            address.city_with_type,
+            //            address.street_with_type,
+            //            address.house,
+            //            address.flat,
+            //        };
+            //}
 
 
 
@@ -42,13 +74,39 @@ namespace DadataCityCacheService.Controllers
 
 
             return Ok(
-                address.fias_id + "," +
-                address.region_with_type + "," +
-                address.city_with_type + "," +
-                address.street_with_type + "," +
-                address.house + "," +
-                address.flat
+                new string[]
+                {
+                    "fdsfd"
+                }
+                //address.fias_id + "," +
+                //address.region_with_type + "," +
+                //address.city_with_type + "," +
+                //address.street_with_type + "," +
+                //address.house + "," +
+                //address.flat
                 ); 
         }
+
+        public async Task<bool> CheckResponseCorrect()
+        {
+            return true;
+        }
+
+        public bool TryGetCachedInfo(Address address, out CityInfoOnly cityInfoOnly)
+        {
+            cityInfoOnly = new()
+            {
+
+            };
+
+            return true;
+        }
+
+
+        public async Task<bool> CheckThisInfoCityOnly(Address address)
+        {
+            return address.city_fias_id.Equals(address.fias_id);
+        }
+
     }
 }
---
+++ DadataCityCacheService/Models/CityInfoOnly.cs
@@ -0,0 +1,20 @@
+﻿namespace DadataCityCacheService.Models
+{
+    public class CityInfoOnly
+    {
+        public string fias_id { get; set; }
+
+        public string geo_lat { get; set; }
+
+        public string geo_lon { get; set; }
+
+        public string result { get; set; }
+
+        public string timezone { get; set; }
+
+        public string[] ToArray()
+        {
+            return new[] { fias_id, geo_lat, geo_lon, result, timezone };
+        }
+    }
+}
