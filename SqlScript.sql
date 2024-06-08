06018a64d2a6529932d982f11c3e5600500132fb
akhmetov_rr <akhmetov_rr@nartis.ru>
2024-06-08 14:55:47 +0300

    sql script edit

--- SqlScript.sql
+++ SqlScript.sql
@@ -1,7 +1,7 @@
 CREATE TABLE cities (
-fias_id varchar(255) PRIMARY KEY,
+fias_id varchar(50) PRIMARY KEY,
 result nvarchar(255) not null,
-geo_lat varchar(255),
-geo_lon varchar(255),
-timezone varchar(50)
+geo_lat varchar(50) not null,
+geo_lon varchar(50) not null,
+timezone varchar(50) not null
 );
