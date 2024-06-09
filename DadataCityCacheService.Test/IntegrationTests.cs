e47b6ffefb24319ac5a449dfe49a2245fb76e6ef
akhmetov_rr <akhmetov_rr@nartis.ru>
2024-06-09 15:42:52 +0300

    test modified

--- DadataCityCacheService.Test/IntegrationTests.cs
+++ DadataCityCacheService.Test/IntegrationTests.cs
@@ -18,19 +18,53 @@ public class IntegrationTests
     [Fact]
     public async Task Addresses_GetAddressInfo_ShouldReturnCityOnlyInfo()
     {
+        List<string> cities = new()
+        {
+            "Москва",
+            "Санкт-Петербург",
+            "Новосибирск",
+            "Екатеринбург",
+            "Казань",
+            "Нижний Новгород",
+            "Челябинск",
+            "Самара",
+            "Омск",
+            "Ростов-на-Дону",
+            "Уфа",
+            "Красноярск",
+            "Воронеж",
+            "Пермь",
+            "Волгоград",
+            "Краснодар",
+            "Саратов",
+            "Тюмень",
+            "Ижевск",
+            "Барнаул"
+        };
 
-        var content = new StringContent(JsonSerializer.Serialize("москва"), Encoding.UTF8, "application/json");
+        foreach(var city in cities)
+        {
+            var content = new StringContent(JsonSerializer.Serialize(city), Encoding.UTF8, "application/json");
 
-        var response = await _client.PostAsync(_url, content);
+            var response = await _client.PostAsync(_url, content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var resultResponseArray = JsonSerializer.Deserialize<string[]>(responseContent);
 
-        Assert.True(response.IsSuccessStatusCode); // Проверяем, что статус-код успешен
-        var responseContent = await response.Content.ReadAsStringAsync();
-        Assert.NotEmpty(responseContent); // Проверяем, что контент не пустой
+            Assert.NotEmpty(responseContent);
+            Assert.True(response.IsSuccessStatusCode);
+            //Assert.True(resultResponseArray.Length == 5);
+            //Assert.True(!resultResponseArray.ToList<string>().Contains(null));
 
-        var responseCity = JsonSerializer.Deserialize(responseContent, typeof(City));
+            await Task.Delay(500);
+
+            
 
 
 
-
+        }
     }
+
+
+
+
 }
